6868e35e28978df7648c848fcc7522df314dc530
mikhovr <programmishka@gmail.com>
2015-06-14 16:25:09 +0300

    Update db_to_csv.py

--- db_to_csv.py
+++ db_to_csv.py
@@ -5,7 +5,7 @@ import re
  
 conn = sqlite3.connect('D:/cw/tweets_april.db')
 c = conn.cursor()
-tw = pd.read_sql("SELECT content from tweets LIMIT 100", conn)
+tw = pd.read_sql("SELECT content from tweets", conn)
 #http://shahmirj.com/blog/extracting-twitter-usertags-using-regex
 posts = tw.ix[:,0].replace([u'(?<=^|(?<=[^a-zA-Z0-9-_\\.]))(@|#)([A-Za-zа-яА-Я0-9]+[A-Za-zа-яА-Я0-9_]+)\s',
                             '\n', 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+',
